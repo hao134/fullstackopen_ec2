8d7ae984b672ae88107c98ae43faccf051beddd8
hao134 <hsh319ab@gmail.com>
2024-12-12 11:09:41 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+
+let persons = 
+[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phone book has info for ${persons.length} people</p>
+        <p>${new Date()}</p>`
+    );
+})
+
+const PORT = 3002
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
